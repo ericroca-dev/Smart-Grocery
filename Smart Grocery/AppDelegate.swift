1bd5a68ac104cb4fbc4764f492f5bec8815eb338
ericroca-dev <ericroca.dev@gmail.com>
2019-05-20 00:20:58 +0300

    Added Google Maps API

--- Smart Grocery/AppDelegate.swift
+++ Smart Grocery/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyA67ffT3MzGmx2_r-_EWaZabEMxT0wXAZk")
+        GMSPlacesClient.provideAPIKey("AIzaSyA67ffT3MzGmx2_r-_EWaZabEMxT0wXAZk")
         return true
     }
 
